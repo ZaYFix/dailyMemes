5265cc9a70e875415dc8212191d20719525371ee
Théo Ackermann <38220718+ZaYFix@users.noreply.github.com>
2020-09-25 10:30:29 +0200

    fix rm & format

--- dailyMemes.sh
+++ dailyMemes.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-rm index.html
-rm *.png
-rm *.jpg
+shopt -s extglob
+rm -v !("dailyMemes.sh")
 
 wget https://www.reddit.com/r/memes/top/
 touch jaj | grep 'alt="Post image"' index.html | head -n 2 | grep 'src' > jaj
-touch joj
-grep -m 1 -oP '(?<=src=").*?(?=" style)' jaj  | head -1 > joj
-touch jij
-sed 's/amp;//g' joj > jij
-cat joj
-cat  jij
+touch joj | grep -m 1 -oP '(?<=src=").*?(?=" style)' jaj  | head -1 > joj
+touch jij | sed 's/amp;//g' joj > jij
 wget -i jij
 nameImg=$(find . -type f -name '*.png*' -o -name '*.jpg*')
-steak=$(readlink -f $nameImg)
+realpathImg=$(readlink -f $nameImg)
 
-gsettings set org.gnome.desktop.background picture-uri $steak
+gsettings set org.gnome.desktop.background picture-uri $realpathImg
 gsettings set org.gnome.desktop.background picture-options "scaled"
